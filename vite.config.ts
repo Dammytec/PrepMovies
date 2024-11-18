8471863eb23eb6c78a582ea7a1de9a978cd42ff6
unknown <dhawau29@gmail.com>
2024-11-18 21:27:03 +0100

    made changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import envCompatible from 'vite-plugin-env-compatible';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , envCompatible()],
-  server:({port:4000})
-})
+  plugins: [react(), envCompatible()],
+  server: {
+    port: 4000, // Correct way to set the server port
+  },
+});
+
